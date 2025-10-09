61280d4037f175edd4463ca3262ebab2e58c91a2
Jared <jared.g.gibson32@gmail.com>
2025-10-08 19:54:46 -0600

    added create_capsules component

--- src/create_capsules/create_capsules.jsx
+++ src/create_capsules/create_capsules.jsx
@@ -2,8 +2,29 @@ import React from 'react';
 
 export function CreateCapsules() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>create capsules displayed here</div>
+    <main>
+        <h2 class="text">Create a Capsule!</h2>
+        <form>
+            <div>
+                <input type="text" placeholder="Enter Title of Experience Here" size="50"/>
+            </div>
+            <br/>
+            <div>
+                <input type="text" placeholder="Journal about Experience Here" size="50"/>
+            </div>
+            <br/>
+            <div>
+                <p class="text">Insert any pictures here: </p><input type="file" />
+            </div>
+            <br/>
+            <div>
+                <p class="text">Enter date this capsule will open: </p><input type="date" />
+            </div>
+            <br/>
+            <div>
+                <input type="submit" />
+            </div>
+        </form>
     </main>
   );
 }

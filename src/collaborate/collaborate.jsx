99d8b38cc78a84de8232828b80eca962aded2d0e
Jared <jared.g.gibson32@gmail.com>
2025-10-08 19:52:32 -0600

    added collaborate component

--- src/collaborate/collaborate.jsx
+++ src/collaborate/collaborate.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 
 export function Collaborate() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>collaborate displayed here</div>
-    </main>
+    <main>
+            {/*This is a websocket interaction*/}
+            <h2 class="text">Social Page</h2>
+            <form>
+                <p class="text">Connect with friend: 
+                    <input type="text" placeholder="Type their name here"/> 
+                    <input type="submit"/></p>        
+            </form>
+            <p class="text">View friend requests:</p>
+            <p class="text">Jake</p>
+            <br/>
+            <p class="text">View friend's capsules:</p>
+            <p class="text"><a href="under_construction.html">Eli's Trip</a></p>
+        </main>
   );
 }

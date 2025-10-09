09787360db8f78d5f825b9d23b6e669b5cde854e
Jared <jared.g.gibson32@gmail.com>
2025-10-08 19:53:23 -0600

    added create_account component

--- src/collaborate/collaborate.jsx
+++ src/collaborate/collaborate.jsx
@@ -3,18 +3,18 @@ import React from 'react';
 export function Collaborate() {
   return (
     <main>
-            {/*This is a websocket interaction*/}
-            <h2 class="text">Social Page</h2>
-            <form>
-                <p class="text">Connect with friend: 
-                    <input type="text" placeholder="Type their name here"/> 
-                    <input type="submit"/></p>        
-            </form>
-            <p class="text">View friend requests:</p>
-            <p class="text">Jake</p>
-            <br/>
-            <p class="text">View friend's capsules:</p>
-            <p class="text"><a href="under_construction.html">Eli's Trip</a></p>
-        </main>
+        {/*This is a websocket interaction*/}
+        <h2 class="text">Social Page</h2>
+        <form>
+            <p class="text">Connect with friend: 
+                <input type="text" placeholder="Type their name here"/> 
+                <input type="submit"/></p>        
+        </form>
+        <p class="text">View friend requests:</p>
+        <p class="text">Jake</p>
+        <br/>
+        <p class="text">View friend's capsules:</p>
+        <p class="text"><a href="under_construction.html">Eli's Trip</a></p>
+    </main>
   );
 }
--- src/create_account/create_account.jsx
+++ src/create_account/create_account.jsx
@@ -2,8 +2,27 @@ import React from 'react';
 
 export function CreateAccount() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>create_account displayed here</div>
-    </main>
+    <main>
+            <h3 class="text">Create an Account</h3>
+            <form method="get" action="html/post_login.html">
+                <div class="login-prompt">
+                    <div class="input-group mb-4">
+                        <span class="text">Name:</span>
+                        <input class="form-control" type="text" placeholder="name" />
+                    </div>
+                    <div class="input-group mb-4">
+                        <span class="text">Username:</span>
+                        <input class="form-control" type="text" placeholder="username" />
+                    </div>
+                    <div class="input-group mb-4">
+                        <span class="text">Password:</span>
+                        <input class="form-control" type="password" placeholder="password" />
+                    </div>
+                    <button type="submit" class="btn btn-primary">Login</button>
+                </div>
+                
+            </form>
+            <p class="text">Return to <a href="../index.html">login</a> page.</p>
+        </main>
   );
 }

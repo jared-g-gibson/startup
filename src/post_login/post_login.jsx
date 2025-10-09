8ee89ca93c78d5170653b0ba13bd92accb58f63b
Jared <jared.g.gibson32@gmail.com>
2025-10-08 19:50:57 -0600

    added post login component

--- src/post_login/post_login.jsx
+++ src/post_login/post_login.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 export function PostLogin() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>post login displayed here</div>
-    </main>
+    <main>
+            <h2 class="text">Welcome, Jared! Click the links below to get started</h2>
+            <ul class="my-list">
+                <li class="lists"><a class="linker" href="create_capsules.html">Create a Capsule</a></li>
+                <li class="lists"><a class="linker" href="view_capsules.html">View Capsules</a></li>
+                <li class="lists"><a class="linker" href="collaborate.html">Social</a></li>
+            </ul>
+        </main>
   );
 }

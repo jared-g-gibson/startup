704ffd5e0c6f7069f15c2dea5ab7b45ba043b075
Jared <jared.g.gibson32@gmail.com>
2025-11-05 15:53:52 -0700

    edited index.js to handle endpoints. will change it soon because they are similar to simon endpoints

--- service/index.js
+++ service/index.js
@@ -1,4 +1,90 @@
 const express = require('express');
-const port = process.argv.length > 2 ? process.argv[2] : 4000;
+const app = express();
 
-app.use(express.static('public'));
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const uuid = require('uuid');
+app.use(express.json());
+
+let users = [];
+let scores = [];
+
+let apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+
+app.get('/', (_req, res) => {
+  res.send({ msg: 'Simon service' });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
+// CreateAuth a new user
+apiRouter.post('/auth/create', async (req, res) => {
+  if (await findUser('email', req.body.email)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await createUser(req.body.email, req.body.password);
+
+    setAuthCookie(res, user.token);
+    res.send({ email: user.email });
+  }
+});
+
+// GetAuth login an existing user
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await findUser('email', req.body.email);
+  if (user) {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      user.token = uuid.v4();
+      setAuthCookie(res, user.token);
+      res.send({ email: user.email });
+      return;
+    }
+  }
+  res.status(401).send({ msg: 'Unauthorized' });
+});
+
+// DeleteAuth logout a user
+apiRouter.delete('/auth/logout', async (req, res) => {
+  const user = await findUser('token', req.cookies[authCookieName]);
+  if (user) {
+    delete user.token;
+  }
+  res.clearCookie(authCookieName);
+  res.status(204).end();
+});
+
+// Middleware to verify that the user is authorized to call an endpoint
+const verifyAuth = async (req, res, next) => {
+  const user = await findUser('token', req.cookies[authCookieName]);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+};
+
+// GetScores
+apiRouter.get('/scores', verifyAuth, (_req, res) => {
+  res.send(scores);
+});
+
+// SubmitScore
+apiRouter.post('/score', verifyAuth, (req, res) => {
+  scores = updateScores(req.body);
+  res.send(scores);
+});
+
+// Default error handler
+app.use(function (err, req, res, next) {
+  res.status(500).send({ type: err.name, message: err.message });
+});
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+  res.sendFile('index.html', { root: 'public' });
+});
